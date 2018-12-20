24776ba065ac52b70414dc47d4f639cf5f34a723
Mike Dilley <mike.dilley@gmail.com>
2018-12-20 19:45:17 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -24,7 +24,7 @@ class PetsController < ApplicationController
   end
 
   get '/pets/:id/edit' do
-    binding.pry
+    # binding.pry
     @pet = Pet.find(params[:id])
     @owners = Owner.all
     erb :'/pets/edit'
