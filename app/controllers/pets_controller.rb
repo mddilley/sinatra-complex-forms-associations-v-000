fe2915cc3b43de958cec09d753ce0b11f88ccb68
Mike Dilley <mike.dilley@gmail.com>
2018-12-20 20:21:15 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,7 +33,7 @@ class PetsController < ApplicationController
   patch '/pets/:id' do
     @pet = Pet.find_by(:id => params[:id])
     @pet.update(params[:pet])
-    if params[:owner][:name].empty?
+    if params[:owner][:name] != ""
       @pet.owner = Owner.create(:name => params[:owner][:name])
       @pet.save
     end
