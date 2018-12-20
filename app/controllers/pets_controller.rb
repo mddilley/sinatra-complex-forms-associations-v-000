49089cb18417bd8944b0c6cb301611a60ba74877
Mike Dilley <mike.dilley@gmail.com>
2018-12-20 19:27:01 +0000

    Automatically backed up by Learn

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,30 @@
+class PetsController < ApplicationController
+
+  get '/pets' do
+    @pets = Pet.all
+    erb :'/pets/index'
+  end
+
+  get '/pets/new' do
+    @owners = Owner.all
+    erb :'/pets/new'
+  end
+
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      Owner.create(:name => params[:owner][:name]).pets << @pet
+    end
+    redirect to "pets/#{@pet.id}"
+  end
+
+  get '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/show'
+  end
+
+  patch '/pets/:id' do
+
+    redirect to "pets/#{@pet.id}"
+  end
+end
