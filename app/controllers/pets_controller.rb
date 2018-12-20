6e0fb01c5c09753eed1e5639cf1527f4d290b1d7
Mike Dilley <mike.dilley@gmail.com>
2018-12-20 20:06:04 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -37,7 +37,6 @@ class PetsController < ApplicationController
     if params[:owner][:name] != ""
       @pet.owner = Owner.create(:name => params[:owner][:name])
     end
-    binding.pry
     redirect to "pets/#{@pet.id}"
   end
 end
