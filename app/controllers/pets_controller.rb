9a8c54e7ea2cbc7eab9839cb112d9bb38e5e1ebe
Mike Dilley <mike.dilley@gmail.com>
2018-12-20 20:12:47 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -31,12 +31,13 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
+    binding.pry
     @pet = Pet.find_by(:id => params[:id])
     @pet.update(params[:pet])
     if params[:owner][:name].strip != ""
       @pet.owner = Owner.create(:name => params[:owner][:name])
     end
-    binding.pry
+
     redirect to "/pets/#{@pet.id}"
   end
 end
